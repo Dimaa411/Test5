dbe3754d64c897514fa0cc9f5be98fa7e66a14a2
Andreiko Dmytro <144222985+Dimaa411@users.noreply.github.com>
2023-11-23 21:31:06 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,50 @@
+function cutArr(one_arr){
 
+    let firstPartArray = []
+    let secondPartArray = []
+
+    let toHalf = 0
+    let fromHalf = Math.floor(one_arr.length / 2)
+
+    for( toHalf; toHalf < Math.floor(one_arr.length / 2); toHalf = toHalf + 1){
+        firstPartArray.push(one_arr[toHalf])
+    }
+
+    for(fromHalf; fromHalf < one_arr.length; fromHalf = fromHalf + 1){
+        secondPartArray.push(one_arr[fromHalf])
+    }
+
+    if(firstPartArray.length > 1 || secondPartArray.length > 1){
+        return mergeArr(cutArr(firstPartArray), cutArr(secondPartArray))
+    }
+
+    return mergeArr(firstPartArray, secondPartArray)
+}
+
+function mergeArr(arr1, arr2){ 
+    let i = 0
+    let k = 0
+    let resArr = []
+    
+    do{
+        if(arr1[i] !== undefined){
+            if(arr1[i] > arr2[k]){
+                resArr.push(arr2[k])
+                k++
+            }
+            else{
+                resArr.push(arr1[i])
+                i++
+            }
+        }else{
+            resArr.push(arr2[k])
+            k++
+        }
+    }
+    while (i < arr1.length || k < arr2.length)
+    return resArr 
+}
+
+module.exports = {
+    cutArr
+}
